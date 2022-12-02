557a74cdbada945b8866c350167306f4b9829f27
zelord <zelord.kwoun@gmail.com>
2022-12-02 21:39:23 +0900

    [ADD] 2-07

--- myproject/pybo/__init__.py
+++ myproject/pybo/__init__.py
@@ -7,9 +7,6 @@ import config
 db = SQLAlchemy()
 migrate = Migrate()
 
-
-
-
 def create_app():
     app = Flask(__name__)
     app.config.from_object(config)
@@ -20,8 +17,9 @@ def create_app():
     from . import models
 
     # 블루프린트
-    from .views import main_views, question_views
+    from .views import main_views, question_views, answer_views
     app.register_blueprint(main_views.bp)
     app.register_blueprint(question_views.bp)
+    app.register_blueprint(answer_views.bp)
     
     return app
