e45304d414cdce5a951a19b57cb21351ff464baa
zelord <zelord.kwoun@gmail.com>
2022-12-08 00:44:17 +0900

    [ADD]3-03

--- myproject/pybo/__init__.py
+++ myproject/pybo/__init__.py
@@ -22,4 +22,7 @@ def create_app():
     app.register_blueprint(question_views.bp)
     app.register_blueprint(answer_views.bp)
     
+    from .filter import format_datetime
+    app.jinja_env.filters['datetime'] = format_datetime
+    
     return app
